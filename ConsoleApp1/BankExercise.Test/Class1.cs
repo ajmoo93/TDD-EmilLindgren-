64e4febbdd46fd135bd7370ce6ac645343d11db5
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-05-22 11:21:45 +0200

    BankExercise and new projecr

---
+++ ConsoleApp1/BankExercise.Test/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankExercise.Test
+{
+    public class BankTest
+    {
+        //[Test]
+        //public void StartWithXero()
+        //{
+        //    var sut = new BankExercise();
+        //    Assert.AreEqual(1000, sut.Balance)
+
+
+        //}
+        [Test]
+        public void BnkDeposit()
+        {
+            var sut = new BankAccount();
+
+            sut.Deposit(1000);
+            Assert.AreEqual(1000, sut.Balance);
+        }
+
+        public void CanWithdrawMoney()
+        {
+            var sut = new BankAccount();
+            sut.Withdraw(1000);
+            Assert.AreEqual(-1000, sut.Balance, "Can withdraw money");
+        }
+    }
+}
---
+++ ConsoleApp1/BankExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankExercise
+{
+    public class BankAccount
+    {
+      public decimal Balance { get; set; }
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(int amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
