13e0b963714dae74fd11938c4c13c4741b532654
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-05-22 15:03:36 +0200

    Think i need regEx....

--- TDD-Exercises/ValidationEngine .Test/ValidationTests.cs
+++ TDD-Exercises/ValidationEngine .Test/ValidationTests.cs
@@ -10,13 +10,36 @@ namespace ValidationEngine.Test
     public class ValidationTests
     {
         [Test]
-        public void TrueForValidAddress()
+        public void trueforvalidaddress()
         {
             var sut = new EmailValidator();
 
             var istrue = sut.ValidateEmailAddress("exe@mail.com");
 
             Assert.IsTrue(istrue);
+
+
+        }
+        [Test]
+        public void FalseForValidAddress()
+        {
+            var sut = new EmailValidator();
+
+           
+            var IsFalse = sut.ValidateEmailAddress("not@excom");
+            
+            Assert.IsFalse(IsFalse, sut.Email);
+
+        }
+        [Test]
+        public void NullForValidAddress()
+        {
+            var sut = new EmailValidator();
+
+
+            var IsNull = sut.ValidateEmailAddress("");
+
+            Assert.IsFalse(IsNull);
 
         }
     }
--- TDD-Exercises/ValidationEngine/Program.cs
+++ TDD-Exercises/ValidationEngine/Program.cs
@@ -2,21 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ValidationEngine
 {
     public class EmailValidator
     {
+        
         static void Main(string[] args)
         {
            
         }
+        private string Email = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
         public bool ValidateEmailAddress(string address)
         {
 
-            return true;
-
+            if (string.IsNullOrEmpty(address))
+            {
+                return false;
+            }
+            var Match = Regex.IsMatch(address, Email);
+            return Match;
         }
+
+
     }
-}
+        
+    }
