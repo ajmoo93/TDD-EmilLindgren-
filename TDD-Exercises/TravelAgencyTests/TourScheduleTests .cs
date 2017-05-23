1f4bcf3db184606dc1afa1e244c8fd14408adede
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-05-23 11:28:18 +0200

    New PRoject-Kenya

---
+++ TDD-Exercises/TravelAgencyTests/TourScheduleTests .cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency;
+
+namespace TravelAgencyTests
+{
+    [TestFixture]
+    public class TourScheduleTests
+    {
+        [Test]
+        public void CanCreateNewTour()
+        {
+            var sut = new TourSchedule();
+
+            sut.CreateTour("WOW Kenyan Safari", new DateTime(2017, 05, 23), 27);
+
+            var Booking = sut.GetToursFor(new DateTime(2017, 05, 23));
+            Assert.AreEqual(1, Booking.Count, "");
+        }
+    }
+}
