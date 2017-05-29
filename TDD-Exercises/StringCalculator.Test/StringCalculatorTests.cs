da2f449f74432804c6562c63636dabe686d2eecb
ajmoo93 <hoganas_1937_70@hotmail.com>
2017-05-29 09:22:59 +0200

    starting with project 3

---
+++ TDD-Exercises/StringCalculator.Test/StringCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Test
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        private Calculator sut { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            sut = new Calculator();
+        }
+
+        [Test]
+        public void ReturnZeroOrEmptystring()
+        {
+           var result = sut.Add("");
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void 
+    }
+}
---
+++ TDD-Exercises/StringCalculator/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add (string input)
+        {
+
+            int Result = 0;
+            List<char> delemiters = new List<char>() { ',' };
+            if (string.IsNullOrEmpty(input))
+            {
+                return 0;
+            }
+
+
+            return Result;
+        }
+    }
+}
